64d116d02bee28e2983cd9f75bd85ebd6de05e1f
gialuann <ln08101996@gmail.com>
2023-09-21 21:50:37 +0700

    a

--- labs/src/App.js
+++ labs/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Bai2 from './components/clock';
 import Sum from './components/sum'
+import NumGenerator from './components/MaxMin'
 function App() {
   return (
     <div className="App">
       <Bai2/>
       <Sum/>
+      <NumGenerator/>
     </div>
   );
 }
---
+++ labs/src/components/MaxMin/index.js
@@ -0,0 +1,61 @@
+import './index.css'
+import Button from '@mui/material/Button';
+import React, { useState } from "react";
+import TextField from '@mui/material/TextField';
+const { useEffect } = React;
+const NumGenerator = () => {
+    const [min, setMin] = useState(1);
+    const [max, setMax] = useState(10);
+    const [number, setNumber] = useState(1);
+
+    const minChange = (event) => {
+        setMin(event.target.value);
+    };
+
+    const maxChange = (event) => {
+        setMax(event.target.value);
+    };
+
+    const generateNumber = (min, max) => {
+        if (min === max) {
+            return min;
+        }
+
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+
+    const getInputs = () => {
+        if (min > max) {
+            const minTemp = min;
+            const maxTemp = max;
+
+            setMin(maxTemp);
+            setMax(minTemp);
+
+            setNumber(generateNumber(max, min));
+        } else {
+            setNumber(generateNumber(min, max));
+        }
+    };
+    useEffect(() => {
+        setNumber(generateNumber(min, max));
+    }, []);
+    return (
+        <div >
+            <div className='wrapper-first-row'>
+                <div className='wrapper-result'>
+                    <p>{number}</p>
+                </div>
+                <div className='wrapper-nim-max'>
+                    <TextField id="standard-basic" label="min" variant="standard" value={min} onChange={minChange} />
+                    <TextField id="standard-basic" label="max" variant="standard" value={max} onChange={maxChange} />
+                </div>  
+            </div>
+            <div>
+                <Button variant="contained" className='random-button' type="submit" onClick={getInputs}>Generate Number</Button>
+            </div>
+        </div>
+    );
+};
+export default NumGenerator
+// ReactDOM.render(<NumGenerator />, document.getElementById("root"));
